2de432819482b38d5881a18afc3723cecbae4dc3
Josh Rolstad <jrolstad@salesforce.com>
2022-11-01 11:53:50 -0700

    Adding status

--- analytics/queries/all_users_with_managers.sql
+++ analytics/queries/all_users_with_managers.sql
@@ -1,58 +1,60 @@
 select u.id,
-    u.directory,
-    u.name,
-    u.location,
-    u.type,
-    u.objecttype,
-    u.upn,
-    u.givenname,
-    u.surname,
-    trim(u.givenname) || ' ' || trim(u.surname) as fullname,
-    u.email,
-    u.company,
-    u.department,
-    u.title,
+    u.directory as Directory,
+    u.name as Name,
+    u.location as Location,
+    u.type as Type,
+    u.objecttype as ObjectType,
+    u.upn as Upn,
+    u.givenname as GivenName,
+    u.surname as SurName,
+    trim(u.givenname) || ' ' || trim(u.surname) as FullName,
+    u.email as Email,
+    u.company as Company,
+    u.department as Department,
+    u.title as JobTitle,
     case 
         when u.type = 'SAM_MACHINE_ACCOUNT' then 'Machine Account'
         when u.location like '%OU=Service Account%' then 'Service Account'
         when u.location like '%OU=Resource Account%' then 'Resource Account'
         when u.location like '%OU=Admin Account%' then 'Admin Account'
         else 'User Account'
-    end as user_type,
+    end as UserType,
     case 
         when u.type = 'SAM_MACHINE_ACCOUNT' then true
         when u.location like '%OU=Service Account%' then true
         when u.location like '%OU=Resource Account%' then true
         when u.location like '%OU=Admin Account%' then false
         else false
-    end as is_non_human,
+    end as IsNonHumanAccount,
     case
          when u.location like '%OU=Terminated%' then 'Terminated'
          when u.location like '%OU=Active%' then 'Active'
          when u.location like '%OU=Non_Active%' or u.location like '%OU=Disabled%' then 'Disabled'
          when u.type = 'SAM_MACHINE_ACCOUNT' or u.location like '%OU=Service Account%' or u.location like '%OU=Resource Account%' then 'Active'
          else 'Unknown'
-    end as user_status,
-    trim(u.manager)!='' and u.manager is not null as has_manager,
-    trim(um.location)!='' and um.location is not null as has_valid_manager,
-    u.manager as manager_raw,
-    um.location as manager_location,
-    um.name as mamanger_name,
-    um.upn as manager_upn,
-    trim(um.givenname) || ' ' || trim(um.surname) as manager_fullname,
+    end as UserStatus,
+    trim(u.manager)!='' and u.manager is not null as HasManager,
+    trim(um.location)!='' and um.location is not null as HasValidManager,
+    u.manager as ManagerLocationRaw,
+    um.location as ManagerLocation,
+    um.name as ManagerName,
+    um.upn as ManagerUpn,
+    trim(um.givenname) || ' ' || trim(um.surname) as ManagerFullName,
     case 
+        when um.type is null or um.type = '' then null
         when um.type = 'SAM_MACHINE_ACCOUNT' then 'Machine Account'
         when um.location like '%OU=Service Account%' then 'Service Account'
         when um.location like '%OU=Resource Account%' then 'Resource Account'
         when um.location like '%OU=Admin Account%' then 'Admin Account'
         else 'User Account'
-    end as manager_user_type,
+    end as ManagerUserType,
     case
+         when um.location is null or um.location = '' then 'Missing'
          when um.location like '%OU=Terminated%' then 'Terminated'
          when um.location like '%OU=Active%' then 'Active'
          when um.location like '%OU=Non_Active%' or um.location like '%OU=Disabled%' then 'Disabled'
          when um.type = 'SAM_MACHINE_ACCOUNT' or um.location like '%OU=Service Account%' or um.location like '%OU=Resource Account%' then 'Active'
          else 'Unknown'
-    end as manager_status
+    end as ManagerStatus
 from user u
 left join user um on u.manager = um.location
