3d30d2df2a2d64a6258c7061fceeeac56cf3493d
Josh Rolstad <jrolstad@salesforce.com>
2022-11-01 08:00:41 -0700

    Cleaning up

--- src/deployments/infra/dynamo_tables.tf
+++ src/deployments/infra/dynamo_tables.tf
@@ -9,12 +9,3 @@ resource "aws_dynamodb_table" "directories" {
   }
 
 }
-
-resource "aws_secretsmanager_secret" "directories_table" {
-  name = "${local.service_name}_directories_table"
-}
-
-resource "aws_secretsmanager_secret_version" "directories_table" {
-  secret_id     = aws_secretsmanager_secret.directories_table.id
-  secret_string = aws_dynamodb_table.directories.name
-}
