17e7e4c426a68fe58f20a5cc9048e00de87ab704
Josh Rolstad <jrolstad@salesforce.com>
2022-11-01 07:56:44 -0700

    Adding config values

--- src/deployments/infra/configuration.tf
+++ src/deployments/infra/configuration.tf
@@ -6,3 +6,12 @@ resource "aws_secretsmanager_secret_version" "directories_table" {
   secret_id     = aws_secretsmanager_secret.directories_table.id
   secret_string = aws_dynamodb_table.directories.name
 }
+
+resource "aws_secretsmanager_secret" "ingestion_queue" {
+  name = "${local.service_name}_ingestion_queue"
+}
+
+resource "aws_secretsmanager_secret_version" "ingestion_queue" {
+  secret_id     = aws_secretsmanager_secret.ingestion_queue.id
+  secret_string = aws_sqs_queue.signal_ingestion.name
+}
