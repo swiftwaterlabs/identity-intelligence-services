5d7f07f37ea9ccb39051cdf7a5b51bbbef744c19
Josh Rolstad <jrolstad@gmail.com>
2022-11-01 15:27:22 -0700

    Adding Analytical Capabilities (#7)

--- src/deployments/infra/storage.tf
+++ src/deployments/infra/storage.tf
@@ -16,3 +16,21 @@ resource "aws_s3_bucket_public_access_block" "signal_data" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "athena_resources" {
+  bucket = local.athena_bucket_name
+}
+
+resource "aws_s3_bucket_acl" "athena_resources" {
+  bucket = aws_s3_bucket.athena_resources.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "athena_resources" {
+  bucket = aws_s3_bucket.athena_resources.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
