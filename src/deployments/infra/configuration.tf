b713e0772dd44521c4643e2ed6c83cbc12196d54
Josh Rolstad <jrolstad@salesforce.com>
2022-11-01 07:54:06 -0700

    Adding config in secrets manager

--- src/deployments/infra/configuration.tf
+++ src/deployments/infra/configuration.tf
@@ -1,15 +1,3 @@
-resource "aws_dynamodb_table" "directories" {
-  name           = "${local.service_name}_directories"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "Id"
-
-  attribute {
-    name = "Id"
-    type = "S"
-  }
-
-}
-
 resource "aws_secretsmanager_secret" "directories_table" {
   name = "${local.service_name}_directories_table"
 }
