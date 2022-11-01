4c45caccc7f217ff985a21cc3c10e1fb664b3302
Josh Rolstad <jrolstad@salesforce.com>
2022-11-01 10:35:52 -0700

    Adding directory

--- src/deployments/infra/glue_database.tf
+++ src/deployments/infra/glue_database.tf
@@ -32,6 +32,11 @@ resource "aws_glue_catalog_table" "user" {
     }
 
     columns {
+      name = "directory"
+      type = "string"
+    }
+
+    columns {
       name = "name"
       type = "string"
     }
@@ -88,6 +93,62 @@ resource "aws_glue_catalog_table" "user" {
 
     columns {
       name = "title"
+      type = "string"
+    }
+  }
+}
+
+esource "aws_glue_catalog_table" "group" {
+  name          = "group"
+  database_name = aws_glue_catalog_database.object_db.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL              = "TRUE"
+  }
+
+  storage_descriptor {
+    location      = "s3://${local.signal_bucket_name}/group"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "json"
+      serialization_library = "org.apache.hive.hcatalog.data.JsonSerDe"
+
+      parameters = {
+        "serialization.format" = 1
+      }
+    }
+
+    columns {
+      name = "id"
+      type = "string"
+    }
+
+    columns {
+      name = "directory"
+      type = "string"
+    }
+
+    columns {
+      name = "name"
+      type = "string"
+    }
+
+    columns {
+      name = "location"
+      type = "string"
+    }
+
+    columns {
+      name = "type"
+      type = "string"
+    }
+
+    columns {
+      name = "objecttype"
       type = "string"
     }
   }
