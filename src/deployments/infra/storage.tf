30336a68f4a60bc73e6233bdf7da18b1517e8985
Josh Rolstad <jrolstad@salesforce.com>
2022-11-01 11:43:40 -0700

    Adding bucket for athena usage

--- src/deployments/infra/storage.tf
+++ src/deployments/infra/storage.tf
@@ -16,3 +16,21 @@ resource "aws_s3_bucket_public_access_block" "signal_data" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "athena_resources" {
+  bucket = "${local.service_name}_athena"
+}
+
+resource "aws_s3_bucket_acl" "athena_resources" {
+  bucket = aws_s3_bucket.athena_resources.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "athena_resources" {
+  bucket = aws_s3_bucket.athena_resources.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
