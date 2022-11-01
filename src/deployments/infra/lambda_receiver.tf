5767e9a718d65e6115e8596469a5d963f54b1c4a
Josh Rolstad <jrolstad@salesforce.com>
2022-10-31 20:17:34 -0700

    Adding code to build out the infra

---
+++ src/deployments/infra/lambda_receiver.tf
@@ -0,0 +1,28 @@
+data "archive_file" "signal_persistance_lambda_zip" {
+  type        = "zip"
+  source_file = "../../cmd/lambda-directoryobjectreceiver/main"
+  output_path = "main.zip"
+}
+
+resource "aws_lambda_function" "signal_persistance" {
+  function_name = "${local.service_name}_signal_persistance"
+
+  role = aws_iam_role.lambda_exec.arn
+
+  filename          = data.archive_file.signal_persistance_lambda_zip.output_path
+  handler           = "main"
+  source_code_hash  = filebase64sha256(data.archive_file.signal_persistance_lambda_zip.output_path)
+  runtime           = "go1.x"
+  
+}
+
+resource "aws_cloudwatch_log_group" "signal_persistance" {
+  name = "/aws/lambda/${aws_lambda_function.signal_persistance.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_event_source_mapping" "signal_persistance" {
+  event_source_arn = aws_sqs_queue.signal_ingestion.arn
+  function_name    = aws_lambda_function.signal_persistance.arn
+}
---
+++ src/deployments/infra/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  service_name = "identity_intelligence_${var.environment}"
+}
