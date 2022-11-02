ac1cdf4be014bd41efb2a504e826977495e36ee4
Josh Rolstad <jrolstad@salesforce.com>
2022-11-02 08:18:44 -0700

    Adding queries for groups

--- analytics/queries/all_group_members.sql
+++ analytics/queries/all_group_members.sql
@@ -2,5 +2,5 @@ select name as Name,
     location as Location,
     type as Type,
     member as MemberLocation
-from "group"
+from identity_intelligence_prd."group"
 CROSS JOIN UNNEST(members) AS t(member)
